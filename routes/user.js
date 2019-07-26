8fdb65f194eba53bffdf00b06027f1a3375733be
Harry Buck <harry-buck@hotmail.co.uk>
2019-07-26 09:35:28 +0100

    Added encryption for user passwords

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const validateUser = require("../validator/user");
 
 const User = require("../models/user");
+const encrypt = require("../encryption/encrypt");
 
 router.get("/test", (req, res) => res.json({"message": "This is a test message"}));
 
@@ -13,19 +14,15 @@ router.post("/register", (req, res) => {
 	if(!validation.isValid)
         return res.status(400).json(validation.errors);
         
-    console.log(req.body);
+	encrypt(req.body.password).then(hash => {
+		req.body.password = hash;
 
-    res.status(200).json({message:"User details added to DB."});
-
-	// encrypt(req.body.email).then(hash => {
-	// 	//console.log("Hash is: "+hash);
-	// 	req.body.email = hash;
-
-	// 	Item(req.body).save().then(() =>
-    //     	res.end("Item added to DB."))
-	// 		.catch(err => console.log(err));
-			
-	// }).catch(err => (console.log(err)));
+		User(req.body).save().then(() =>
+            res.status(200).json({message:"User details added to DB."}))
+                .catch(err => console.log(err));
+                		
+    }).catch(err => (console.log(err)));
+    
 });
 
 
