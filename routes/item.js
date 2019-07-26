c704ebffad1e2181116f61c7e827c4ed42df7328
Harry Buck <harry-buck@hotmail.co.uk>
2019-07-26 11:36:29 +0100

    Added delete route for item

--- routes/item.js
+++ routes/item.js
@@ -7,8 +7,15 @@ const User = require("../models/user");
 
 const validateItem = require("../validator/item");
 
+
+// @route   GET item/test
+// @desc    Send a test message to the client
+// @access  Public
 router.get("/test", (req, res) => res.json({"message": "This is a test message"}));
 
+// @route   GET item/getAll
+// @desc    Get all items
+// @access  Public
 router.get("/getAll", (req, res) => {
     Item.find({}, '-email')
 	.then(items => {
@@ -22,6 +29,10 @@ router.get("/getAll", (req, res) => {
 	.catch(err => res.status(404).json({Error:"There are no items."}).end());
 });
 
+
+// @route   POST item/add
+// @desc    Add a new item to the database
+// @access  Public
 router.post("/add", (req, res) => {
 	let validation = validateItem(req.body);	
 
@@ -42,6 +53,42 @@ router.post("/add", (req, res) => {
         res.status(400).json({Error:"The username or password entered does not match a username or password in our records."});
     });    
 
+});
+
+
+// @route   DELETE item/delete
+// @desc    Delete an item from the database
+// @access  Public
+router.delete("/delete", (req,res) => {
+
+    Item.findById(req.body._id).then(item => { 
+
+        User.findOne({username: item.username}).then(user => { 
+
+            if(user.email === req.body.email){
+
+                bcrypt.compare(req.body.password, user.password).then(ifMatch => {
+                    if(ifMatch){
+                        item.remove().then(() =>
+                            res.status(200).json({Message:"Item removed from DB."}).end())
+                            .catch(() => res.status(404).json({ Error: "No item found" }));                           
+                    } else {
+                        res.status(400).json({Error:"The username, email, or password entered does not match a set in our records."}).end();
+                    }			
+                });  
+
+            } else {
+                res.status(400).json({Error:"The username, email, or password entered does not match a set in our records."}).end();
+            }    
+
+        }).catch(() => {
+            res.status(400).json({Error:"The username, email, or password entered does not match a set in our records."}).end();
+        });      
+
+    }).catch(() => {
+        res.status(400).json({Error:"The username, email, or password entered does not match a set in our records."}).end();
+    }); 
+  	
 });
 
 
