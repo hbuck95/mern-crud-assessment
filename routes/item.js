2ae0e8348b2fed30e5e86bfc7d6b61406f8248fb
Harry Buck <harry-buck@hotmail.co.uk>
2019-07-26 11:00:13 +0100

    Added route to get all items

--- routes/item.js
+++ routes/item.js
@@ -9,4 +9,18 @@ const validateItem = require("../validator/item");
 
 router.get("/test", (req, res) => res.json({"message": "This is a test message"}));
 
+router.get("/getAll", (req, res) => {
+    Item.find({}, '-email')
+	.then(items => {
+		if (items === undefined || items.length == 0) {
+			errors.noItems = "There are no items!";
+			res.status(404).json({Error:"There are no items."}).end();
+		} else {
+            res.status(200).json(items).end();
+        }
+	})
+	.catch(err => res.status(404).json({Error:"There are no items."}).end());
+});
+
+
 module.exports = router;
